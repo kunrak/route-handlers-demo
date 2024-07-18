b850749811b1d0bd6df97ee4d3fc6226d331aacb
kunrak <rakeshkunducsgi@gmail.com>
2024-07-18 15:40:03 +0530

    tut 41

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,7 +1,12 @@
+import { type NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET(request: Request) {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query")
+    const filteredComments = query ?
+        comments.filter((comment) => comment.text.includes(query)) : comments;
+    return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-    return new Response("Profile API Data")
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+
+    return new Response("<h1>Profile API Data</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        }
+    });
 }
